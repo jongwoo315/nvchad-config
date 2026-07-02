ba63d4ac1801c788948e6237d017ed80e792edac
jongwoo315 <jongwoo315@gmail.com>
2026-07-03 01:44:19 +0900

    fix: 리사이즈 시 nvim-tree 폭 고정 (winfixwidth + equalalways off)

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,9 @@
 require "nvchad.autocmds"
+
+-- keep nvim-tree width fixed when other windows resize
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "NvimTree",
+  callback = function()
+    vim.wo.winfixwidth = true
+  end,
+})
--- lua/options.lua
+++ lua/options.lua
@@ -5,6 +5,9 @@ require "nvchad.options"
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
+-- resize/split 시 다른 창 자동 균등화 금지 (nvim-tree 폭 유지)
+vim.o.equalalways = false
+
 -- treesitter folding: za toggle, zM close all, zR open all, zm/zr by level
 vim.o.foldmethod = "expr"
 vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
